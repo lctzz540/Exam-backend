5ed7254d375057698d7aa22f1fa1e4fa0b3172ae
Lê Công Thắng <80148108+lctzz540@users.noreply.github.com>
2022-11-07 01:05:16 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, UploadFile
 import uvicorn
 from controller import processFileUpload
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def hello():
