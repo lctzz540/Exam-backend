cbb9cf20f73ec98f98b2708721b0a9f713599181
Lê Công Thắng <80148108+lctzz540@users.noreply.github.com>
2022-11-07 13:15:06 +0700

    Update api.py

--- api.py
+++ api.py
@@ -21,7 +21,7 @@ def hello():
     return 'This app is running'
 
 @app.post("/upload-file")
-async def create_upload_file(files: list[UploadFile]):
+async def create_upload_file(files: list[UploadFile], n: int):
    return await processFileUpload(files)
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
